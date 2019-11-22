f5569efd6382e5bfb694967c0e34a3b22ac6a05d
yuslianto <yuslianto56@gmail.com>
2019-11-22 20:19:14 +0700

    instal react-native-vector-icons in react-native version 0.61.4 solve for android with add apply from: ../../node_modules/react-native-vector-icons/fonts.gradle to android/app/buildgradle

--- App.js
+++ App.js
@@ -5,10 +5,23 @@ import {
   StyleSheet
 } from "react-native";
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const App = (props) => {
   return (
     <View style={styles.container}>
       <Text>screen app</Text>
+      <Icon.Button name="facebook" backgroundColor="#3b5998">
+        <Text style={{ fontFamily: 'Arial', fontSize: 15 }}>
+          Login with Facebook
+        </Text>
+      </Icon.Button>
+      <Icon.Button
+        name="facebook"
+        backgroundColor="#3b5998"
+      >
+        Login with Facebook
+      </Icon.Button>
     </View>
   );
 };
